9b578b1f2eec2c0c89c8b2d217605a73699f7a57
rinoxx1 <mohamed3boughda@gmail.com>
2023-12-12 13:05:43 -0800

    simple shell

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_COMMAND_LENGTH 100
+#define MAX_PATH_LENGTH 100
+
+#endif /* SHELL_H */
