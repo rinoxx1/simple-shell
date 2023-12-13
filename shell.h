aeba5e4156ba7d89c1b9440adb3a90de717d2ea4
rinoxx1 <mohamed3boughda@gmail.com>
2023-12-12 17:16:35 -0800

    simple shell

---
+++ shell.h
@@ -0,0 +1,31 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+extern char **environ; /* Expose the environment variable */
+void tokenizeCommand(const char *com, char *com_args[], int *arg_count);
+void constructFullPath(const char *token, const char *command, char *full_com);
+int executeAndAwait(const char *full_com, char *com_args[]);
+int searchCommanInPath(const char *command, char *com_args[], const char *road);
+int executeCommand(const char *com);
+void print_environment(void);
+int fileExists(const char *road, const char *file);
+void handleExitCommand(void);
+void handleEnvCommand(void);
+void handleInvalidCommand(void);
+void handleWordCountError(void);
+
+#define MAX_COMMAND_LENGTH 500
+#define MAX_PATH_LENGTH 500
+
+#endif
